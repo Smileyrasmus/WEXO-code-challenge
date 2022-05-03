6e32cc8ab1e27930f8b4de6e7d0dc4daae8c602e
Rasmus Emil Bøgel Larsen <rasmus-larsen1999@hotmail.dk>
2022-05-03 14:54:48 +0200

    fixed bad behavioer of category dropdown

--- code-challenge/src/components/header/Header.tsx
+++ code-challenge/src/components/header/Header.tsx
@@ -1,20 +1,31 @@
 import { NavLink } from "react-router-dom";
 import NavCategory from "./NavCategory";
+import { useState } from "react"
 
 /**
  * Header component consisting of website name and navigation
  * @returns Header componenet
  */
 export default function Header() {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    /**
+     * Toggles the menuIsOpen state between true or false
+     */
+    function toggleMenu(): void {
+        menuIsOpen ? setMenuIsOpen(false) : setMenuIsOpen(true)
+    }
 
+    function setMenuIsOpenToFalse(): void {
+        setMenuIsOpen(false)
+    }
 
     return (
         <header grid-area="Header">
             <span style={{ fontWeight: "bold", fontSize: 24 }}>Wexo Tickets</span>
             <div className="navigation">
-                <NavLink className="navLink noselect" to="/">Home</NavLink>
-                <NavCategory />
-                <NavLink className="navLink noselect" to="/wishlist">Wishlist</NavLink>
+                <NavLink className="navLink noselect" to="/" onClick={setMenuIsOpenToFalse}>Home</NavLink>
+                <NavCategory toggleMenu={toggleMenu} menuIsOpen={menuIsOpen}/>
+                <NavLink className="navLink noselect" to="/wishlist" onClick={setMenuIsOpenToFalse}>Wishlist</NavLink>
             </div>
         </header>
     )
--- code-challenge/src/components/header/NavCategory.tsx
+++ code-challenge/src/components/header/NavCategory.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { useLocation } from "react-router-dom"
 import { useGlobalContext } from "../../context/GlobalData"
@@ -7,30 +6,28 @@ import { useGlobalContext } from "../../context/GlobalData"
  * A navigation component, with a dropdown menu with links to all categories
  * @returns NavCategory component
  */
-export default function NavCategory() {
+type NavCategoryProps = {
+    toggleMenu: () => void,
+    menuIsOpen: boolean
+}
+
+export default function NavCategory(props: NavCategoryProps) {
     const globalData = useGlobalContext().copy
     
-    const [menuIsOpen, setMenuIsOpen] = useState(false)
 
-    /**
-     * Toggles the menuIsOpen state between true or false
-     */
-    function toggleMenu(): void {
-        menuIsOpen ? setMenuIsOpen(false) : setMenuIsOpen(true)
-    }
 
     return (
         <div>
             <div
-            className={"noselect navLink " + (useLocation().pathname.includes("categories") ? "active" : "")} // if in categories, mark navlink as active
-            onClick={toggleMenu}>Categories</div>
+            className={"noselect navLink " + (useLocation().pathname.includes("categories") || props.menuIsOpen ? "active" : "")} // if in categories, mark navlink as active
+            onClick={props.toggleMenu}>Categories</div>
             <div
                 className="dropdownMenu"
-                style={{display:menuIsOpen ? "grid" : "none"}} // is only rendered when menuIsOpen is true
+                style={{display:props.menuIsOpen ? "grid" : "none"}} // is only rendered when menuIsOpen is true
             >
                 {globalData.genres.map(genre => {
                     genre = genre[0].toUpperCase() + genre.slice(1)
-                    return (<NavLink key={genre} to={`/categories/${genre}`} className="test navLink" onClick={toggleMenu}>{genre}</NavLink>)
+                    return (<NavLink key={genre} to={`/categories/${genre}`} className="test navLink" onClick={props.toggleMenu}>{genre}</NavLink>)
                 })}
             </div>
         </div>
